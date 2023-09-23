5e88d0a71d5c254dbb99d1de3014e24fcf582f78
ErikQuintero <erik_quintero@ciencias.unam.mx>
2023-09-23 01:03:47 -0600

    añadiendo interfaz grafica

--- código/Pruebas.java
+++ código/Pruebas.java
@@ -1,84 +1,119 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JFrame;
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 
 public class Pruebas{
 
 	public static void main (String args []){
-		/*ArrayList<Vertice> vertices = GraficaAl.generaVertices();
-		ArrayList<Arista> aristas = GraficaAl.generaAristas(vertices);
-		System.out.println(vertices);
-		System.out.println("__________________________");
-		System.out.println(aristas);
-		System.out.println("___________________________");
-		ArrayList<Arista> aristas2 = new ArrayList<Arista>();
-		for(Arista aux : aristas){
-			aristas2.add(0,aux);
-		}
-		GraficaAl.eliminaArista(aristas2);
-		System.out.println(aristas2);
-		System.out.println("___________________________");
-		ArrayList<Arista> aristas3 = new ArrayList<Arista>();
-		for(Arista aux : aristas2){
-			aux.orientarA();
-			aristas3.add(0,aux);
-		}
-		System.out.println(aristas3);*/
-		/*Digrafica d = GraficaAl.digraficaAle();
-		System.out.println(d);
-		Digrafica d2 = d.clonar();
-		ArrayList<VerticeD> nucleo = d.getNucleo();
-		System.out.println(nucleo);
-		boolean si = d2.esAbsorbente(nucleo);
-		System.out.println(si);*/
-		VerticeD a = new VerticeD("a");
-		VerticeD b = new VerticeD("b");
-		VerticeD c = new VerticeD("c");
-		VerticeD d = new VerticeD("d");
-		VerticeD e = new VerticeD("e");
-		VerticeD f = new VerticeD("f");
-		VerticeD g = new VerticeD("g");
-		VerticeD h = new VerticeD("h");
-		VerticeD i = new VerticeD("i");
-		VerticeD j = new VerticeD("j");
-		Flecha f1 = new Flecha(b,a);
-		Flecha f2 = new Flecha(b,c);
-		Flecha f3 = new Flecha(d,c);
-		Flecha f4 = new Flecha(d,e);
-		Flecha f5 = new Flecha(c,j);
-		Flecha f6 = new Flecha(e,h);
-		Flecha f7 = new Flecha(j,i);
-		Flecha f8 = new Flecha(h,g);
-		Flecha f9 = new Flecha(g,f);
-		Flecha f10 = new Flecha(i,h);
-		ArrayList<VerticeD> vertex = new ArrayList<VerticeD>();
-		vertex.add(a);
-		vertex.add(b);
-		vertex.add(c);
-		vertex.add(d);
-		vertex.add(e);
-		vertex.add(f);
-		vertex.add(g);
-		vertex.add(h);
-		vertex.add(i);
-		vertex.add(j);
-		ArrayList<Flecha> fle = new ArrayList<Flecha>();
-		fle.add(f1);
-		fle.add(f2);
-		fle.add(f3);
-		fle.add(f4);
-		fle.add(f5);
-		fle.add(f6);
-		fle.add(f7);
-		fle.add(f8);
-		fle.add(f9);
-		fle.add(f10);
 
-		Digrafica di = new Digrafica(vertex,fle);
-		ArrayList<VerticeD> nucleo = di.getNucleo();
-		System.out.println(di);
-		System.out.println(nucleo);
-		boolean si = di.esAbsorbente(nucleo);
-		System.out.println(si);
+		///////////////////////////////////////////
+		String nombreArchivo = "Archivo.txt";
 
-	}
+	   	List<String> vertex = getVertices(nombreArchivo);
+	   	List<String> arrow = getFlechas(nombreArchivo);
+	   	ArrayList<VerticeD> vertices = new ArrayList<VerticeD>();
+	   	ArrayList<Flecha> flechas = new ArrayList<Flecha>();
+	   	for (String s : vertex) {
+		   vertices.add(new VerticeD(s));
+
+	   	}
+	   	for (String ss : arrow) {
+		   String[] a = ss.split(",");
+		   VerticeD ver1 = new VerticeD("Aux1");
+		   VerticeD ver2 = new VerticeD("Aux2");
+		   for (VerticeD v1 : vertices) {
+			   if(a[0].equals(v1.identificador)){
+				   ver1 = v1;
+			   }
+		   }
+		   for (VerticeD v2 : vertices) {
+			   if(a[1].equals(v2.identificador)){
+				   ver2 = v2;
+			   }
+		   }
+		   Flecha f = new Flecha(ver1,ver2);
+		   flechas.add(f);
+	   	}
+	   	Digrafica d = new Digrafica(vertices,flechas);
+
+		//
+		Graph<String, String> grafo = new DirectedSparseGraph<>();
+
+        for(VerticeD v1 : d.vertices){
+			grafo.addVertex(v1.identificador);
+		}
+
+		System.out.println("___________________________");
+		System.out.println(d.flechas);
+		System.out.println("___________________________");
+		System.out.println(d.vertices);
+		int cont = 1;
+		for(Flecha fle1 : d.flechas){
+			String faux = fle1.extremo1 + "," + fle1.extremo2;
+			String[] a = faux.split(",");
+			String saux = "Flecha";
+			saux = saux + cont;
+			grafo.addEdge(saux, a[0], a[1]);
+			cont++;
+		}
+
+        // Crear un layout dirigido
+		CircleLayout<String, String> layout = new CircleLayout<>(grafo);
+
+        // Crear un visualizador dirigido
+        VisualizationViewer<String, String> visualizador = new VisualizationViewer<>(layout);
+
+        // Configurar un decorador para etiquetas
+        visualizador.getRenderContext().setVertexLabelTransformer(v -> v.toString());
+
+        // Crear un frame para la visualización
+        JFrame frame = new JFrame("Visualizacion de Grafo Dirigido");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(visualizador);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+	public static List<String> getVertices(String nombreArchivo) {
+        List<String> elementos = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+			br.readLine();
+			String segundaLinea = br.readLine();
+            if (segundaLinea != null) {
+                String[] elementosArray = segundaLinea.split(",");
+                for (String elemento : elementosArray) {
+                    elementos.add(elemento);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return elementos;
+ 	}
+
+	public static List<String> getFlechas(String nombreArchivo) {
+       List<String> elementos = new ArrayList<>();
+       try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+           String linea;
+           int cont = 0;
+           while ((linea = br.readLine()) != null) {
+               if (cont > 1) {
+                   elementos.add(linea.trim());
+               } else{
+                   cont++;
+               }
+			}
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+       return elementos;
+   }
 
 }
